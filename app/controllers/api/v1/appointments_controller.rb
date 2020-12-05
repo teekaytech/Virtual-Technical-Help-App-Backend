db8b20196737235e08d6b8019016e4a16b0d2dd3
teekaytech <taofeek.olalere@feghas.com>
2020-12-05 11:44:49 +0100

    Make changes to appointments index action

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::AppointmentsController < ApplicationController
 
   def index
     @appointments = logged_in_user.appointments
-    return json_response(@appointments) if @appointments
+    return json_response(full_appointments(@appointments)) if @appointments
 
     error_message
   end
@@ -41,5 +41,21 @@ class Api::V1::AppointmentsController < ApplicationController
 
   def error_message
     render json: { error: 'You have to login.', status: 'NOT_LOGGED_IN' }
+  end
+
+  def full_appointments(appointments)
+    full_appointments = []
+    appointments.each do |app|
+      full_appointments << {
+        date_created: app.created_at,
+        id: app.id,
+        date: app.date,
+        status: app.status,
+        duration: app.duration,
+        engineer: app.engineer[:name],
+        location: app.engineer[:location]
+      }
+    end
+    full_appointments
   end
 end
